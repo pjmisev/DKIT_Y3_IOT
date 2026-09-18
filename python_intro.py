e130a44a38d0cace68f5e685bd1283a679e0456a
pjmisev <pijus.misevicius@outlook.com>
2026-09-18 12:54:50 +0100

    Intro completed

---
+++ python_intro.py
@@ -0,0 +1,68 @@
+import pyttsx3
+
+
+def main():
+    # hello_world()
+    # speak("Leo is an idiot")
+    # get_int()
+    # agree()
+    # test_range()
+    # find_name()
+    phone_book()
+
+
+def hello_world():
+    print("Hello, world")
+
+
+def speak(to_say:str):
+    engine = pyttsx3.init()
+    engine.say(to_say)
+    engine.runAndWait()
+
+
+def get_int():
+    while True:
+        try:
+            number = int(input("Please enter an integer: "))
+            print (number)
+            return
+        except ValueError:
+            print("This is not an integer >:(")
+
+
+def agree():
+    while True:
+            response = input("Do you agree: ").lower()
+            if response in ["yes", "no", "y", "n", "agree", "disagree"]:
+                return
+            else:
+                print("Answer is not agreeing or disagreeing!")
+
+
+def test_range():
+    numbers = list(range(1,101))
+    print(numbers)
+    for i in numbers:
+        if (i%2 == 0):
+            print(i)
+            break
+
+
+def find_name():
+    names = ["Pijus", "Leo", "Raivis", "Anastasija", "Kornel"]
+    name_to_search = input("Who are you looking for?: ").lower()
+    for i in names:
+        if i.lower() == name_to_search:
+            print("Found " + i + "!")
+
+
+def phone_book():
+    people = {"Pijus": "+353123456789", "Leonas": "+353123456789"}
+    name = input("Please enter name:")
+    if name in people:
+        print(f"Number: {people[name]}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/dkit_y3_iot/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from dkit-y3-iot!")
